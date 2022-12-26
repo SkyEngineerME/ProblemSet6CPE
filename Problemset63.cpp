73fe2601a3f4add38af05e18bf2926d57cd959e5
SkyEngineerME <guymechatronic@gmail.com>
2022-12-26 21:01:45 +0700

    63Done

---
+++ Problemset63.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x,y,z;
+    x = 10/5%2+10/2*10;
+    y = 2*2/2+2/2*2-2;
+    z = 69%10%5%3;
+
+    cout << x << ' ' << y << ' '<< z;
+    return 0;
+}
